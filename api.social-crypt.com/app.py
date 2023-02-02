b3445d348e7a722c3095c35dd31f8969b66ae68c
hrishikesh332 <hriskikesh.yadav332@gmail.com>
2023-02-03 01:03:00 +0530

    feat: vader sentiment analyser

--- api.social-crypt.com/app.py
+++ api.social-crypt.com/app.py
@@ -1,6 +1,7 @@
 from io import BytesIO
-from flask import Flask 
+from flask import Flask, jsonify
 import os
+
 import tweepy
 from dotenv import load_dotenv
 from flask import request,jsonify
@@ -17,6 +18,7 @@ import numpy as np
 import base64
 # from flask import send_file
 from flask import send_file
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 app = Flask(__name__)
 
@@ -93,6 +95,37 @@ def query(payload):
 	return response.json()
 
 
+
+@app.route('/sentiment')
+def sentiment():
+
+	
+    sid_obj = SentimentIntensityAnalyzer()
+    
+    sentiment_dict = sid_obj.polarity_scores([sentence])
+    print(sentiment_dict['neg']*100, "% Negative")
+    print(sentiment_dict['pos']*100, "% Positive")
+    print("Review Overall Analysis", end = " ")
+
+    senti=[]
+    if sentiment_dict['compound'] >= 0.05 :
+        senti.append("Positive")
+    elif sentiment_dict['compound'] < 0.05 :
+        senti.append("Negative")
+    else :
+        senti.append("Neutral")
+    print(senti)
+    return jsonify({"result":senti})
+    
+
+
+    
+
+
+
+
+
+
 @app.route('/news')
 def news():
     url = 'https://blogs.jayeshvp24.dev/dive-into-web-design'
@@ -130,65 +163,65 @@ def plotly_wordcloud2():
     return send_file("./wordcloud.png", mimetype='image/png')
     # return render_template('plot.html', plot_url=plot_url)
 
-@app.route('/cloud')
-def plotly_wordcloud():
-    url = 'https://blogs.jayeshvp24.dev/dive-into-web-design'
-    goose = Goose()
-    articles = goose.extract(url)
-    text = query({
-	"inputs":  articles.cleaned_text
-    })
-    wc = WordCloud(stopwords = set(STOPWORDS),
-                   max_words = 200,
-                   max_font_size = 100)
-    wc.generate(text[0]['summary_text'])
+# @app.route('/cloud')
+# def plotly_wordcloud():
+#     url = 'https://blogs.jayeshvp24.dev/dive-into-web-design'
+#     goose = Goose()
+#     articles = goose.extract(url)
+#     text = query({
+# 	"inputs":  articles.cleaned_text
+#     })
+#     wc = WordCloud(stopwords = set(STOPWORDS),
+#                    max_words = 200,
+#                    max_font_size = 100)
+#     wc.generate(text[0]['summary_text'])
     
-    word_list=[]
-    freq_list=[]
-    fontsize_list=[]
-    position_list=[]
-    orientation_list=[]
-    color_list=[]
+#     word_list=[]
+#     freq_list=[]
+#     fontsize_list=[]
+#     position_list=[]
+#     orientation_list=[]
+#     color_list=[]
 
-    for (word, freq), fontsize, position, orientation, color in wc.layout_:
-        word_list.append(word)
-        freq_list.append(freq)
-        fontsize_list.append(fontsize)
-        position_list.append(position)
-        orientation_list.append(orientation)
-        color_list.append(color)
+#     for (word, freq), fontsize, position, orientation, color in wc.layout_:
+#         word_list.append(word)
+#         freq_list.append(freq)
+#         fontsize_list.append(fontsize)
+#         position_list.append(position)
+#         orientation_list.append(orientation)
+#         color_list.append(color)
         
-    # get the positions
-    x=[]
-    y=[]
-    for i in position_list:
-        x.append(i[0])
-        y.append(i[1])
+#     # get the positions
+#     x=[]
+#     y=[]
+#     for i in position_list:
+#         x.append(i[0])
+#         y.append(i[1])
             
-    # get the relative occurence frequencies
-    new_freq_list = []
-    for i in freq_list:
-        new_freq_list.append(i*100)
-    new_freq_list
+#     # get the relative occurence frequencies
+#     new_freq_list = []
+#     for i in freq_list:
+#         new_freq_list.append(i*100)
+#     new_freq_list
     
-    trace = go.Scatter(x=x, 
-                       y=y, 
-                       textfont = dict(size=new_freq_list,
-                                       color=color_list),
-                       hoverinfo='text',
-                       hovertext=['{0}{1}'.format(w, f) for w, f in zip(word_list, freq_list)],
-                       mode='text',  
-                       text=word_list
-                      )
+#     trace = go.Scatter(x=x, 
+#                        y=y, 
+#                        textfont = dict(size=new_freq_list,
+#                                        color=color_list),
+#                        hoverinfo='text',
+#                        hovertext=['{0}{1}'.format(w, f) for w, f in zip(word_list, freq_list)],
+#                        mode='text',  
+#                        text=word_list
+#                       )
     
-    layout = go.Layout({'xaxis': {'showgrid': False, 'showticklabels': False, 'zeroline': False},
-                        'yaxis': {'showgrid': False, 'showticklabels': False, 'zeroline': False}})
+#     layout = go.Layout({'xaxis': {'showgrid': False, 'showticklabels': False, 'zeroline': False},
+#                         'yaxis': {'showgrid': False, 'showticklabels': False, 'zeroline': False}})
     
-    fig = go.Figure(data=[trace], layout=layout)
-    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-    print(graphJSON)
-    print(type(fig))
-    return graphJSON
+#     fig = go.Figure(data=[trace], layout=layout)
+#     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+#     print(graphJSON)
+#     print(type(fig))
+#     return graphJSON
 
 
 if __name__ == '__main__':
